2273ef04214583865456f0567aa43039151cf9b9
Daniel Krol <orblivion@gmail.com>
2026-02-06 17:11:44 -0500

    Adjust city format. Many side quests.
    Return a city as a city name, admin1 name, country (code, for now).
    
    Streamline how I approach dupes, including how it relates to cities without admin1 codes.
    
    Probably other things I'm not acknowledging.
    
    Lots of cleanups.

--- build-databases/static/import-places.py
+++ build-databases/static/import-places.py
@@ -1,6 +1,9 @@
 import csv, gzip, regex, requests, json, os, tarfile, unicodedata, zipfile
 from io import BytesIO, StringIO, TextIOWrapper
-from collections import defaultdict
+from collections import defaultdict, namedtuple
+
+# `city_identifier` uniquely identifies a city
+city_identifier = namedtuple("city_identifier", ["name", "admin1", "country"])
 
 # Turn geojsons and geonames city csv into our trimmed down csv format for later search import
 # Note that geonames is not OSM data. This may be an alternative if we ever care: https://wiki.openstreetmap.org/wiki/OSMNames
@@ -54,7 +57,7 @@ class CitiesRow(Row):
 # Download the admin1codes from geonames.
 # Return: {country_code: {admin1_code: admin1_name}}
 #   I.e.: {"US":         {"MN":        "Minnesota"}}
-def get_admin1_mapping():
+def fetch_admin1_names():
 
     # https://download.geonames.org/export/dump/
     # admin1CodesASCII.txt: names in English for admin divisions. Columns: code, name, name ascii, geonameid
@@ -65,81 +68,114 @@ def get_admin1_mapping():
     admin1_csv_f = StringIO(requests.get("https://download.geonames.org/export/dump/admin1CodesASCII.txt").content.decode('utf-8'))
     admin1_reader = csv.reader(admin1_csv_f, delimiter='\t')
 
-    admin1_mapping = defaultdict(dict)
+    admin1_names = defaultdict(dict)
     for row_ in admin1_reader:
         row = Admin1Row(row_)
         # row['code'] looks something like: "AD.06"
         country_code, admin1_code = row['code'].split('.')
 
-        assert row["name"] not in admin1_mapping[country_code].values(), "Duplicate admin1 within country: " + repr(row)
-        admin1_mapping[country_code][admin1_code] = row["name"]
+        assert row["name"] not in admin1_names[country_code].values(), "Duplicate admin1 within country: " + repr(row)
+        admin1_names[country_code][admin1_code] = row["name"]
 
-    return admin1_mapping
+    return admin1_names
 
-def assert_city_has_country(city_row):
+def validate_city_row(city_row):
     # Make sure all cities have a country code (Adding this here because some
     # things in the data aren't 100% as expected. I just wanted to make sure
     # that this isnt' one of those things).
     assert row["country_code"], "Oops, I guess some cities don't have a country: " + repr(row)
 
-# Make sure we have no countries where some cities have Admin1 codes
-# and and some do not. It would create an inconsistent experience
-# with two formats ("City, Admin1, CountryCode" vs "City, CountryCode")
-# especially if there are duplicate city names with different formats.
-#
-# Fortunately it's only in rare cases that this inconsistency happens.
-# I'll just name them here, and remove the admin1_code for those
-# countries. In theory, removing Admin1 could inadvertently create more
-# duplicates in the process. We'll confirm that we didn't do this.
-#
-# TODO - Assert that the admin1_problem_country_codes countries actually
-# have both formats. If not, we can remove it from the list.
-# TODO - Comment that sometimes we remove admin1_code if it doesn't point to anything.
-admin1_problem_country_codes = ["MR"] # TODO - try removing this
-
-# Remove admin1_code if either:
-# * It's a "problem country" where not all cities have admin1_codes
-# * It doesn't refer to anything in the admin1_mapping
-def clean_admin1_code_for_city(city_row):
-    admin1_code = row["admin1_code"]
-    if admin1_code not in admin1_mapping[row["country_code"]]:
-        # The city claims to have an admin1_code and country_code combination that isn't in
-        # admin1_mapping.  don't know what to do with it so let's just say it has no admin1_code.
-        print("admin1_code " + row["admin1_code"] + " not found for country " + row["country_code"])
-        row["admin1_code"] = ""
-    if row["country_code"] in admin1_problem_country_codes:
-        # The city claims to have a country_code that doesn't hvae
-        # We don't know what to do with it so let's just say it has no admin1_code.
-        row["admin1_code"] = ""
-
-# Check that each country either has admin1codes for all
-# cities, or for no cities (after we remove some of them
-# in clean_admin1_code_for_city)
-countries_have_admin1_code = {}
-def check_country_admin1_consistency(city_row):
-    has_admin1_code = bool(row["admin1_code"])
-    if countries_have_admin1_code.get(row["country_code"]) == (not has_admin1_code):
-        raise Exception(row["country_code"] + " has some but not all cities without admin1_code")
-    countries_have_admin1_code[row["country_code"]] = has_admin1_code
-
-# TODO - actually don't put the country and state name in the queryable stuff? It'll grab a bunch of unwanted things. Maybe they should be different fields.
-#    * That way I can add admin2 admin3 admin4 etc etc. Though don't if not necessary to dedupe?
-#    * I guess it's "match name, then match admin1 or admin2 or country etc."
-#    * But it should continue to show the full name because the geocoder thing displays it well.
-def city_display_name(row):
-    if row["admin1_code"]:
-        # We have the admin1_code, include it in the display_name
-        return ", ".join([
-            row["name"],
-            admin1_mapping[row["country_code"]][row["admin1_code"]],
-            row["country_code"]
-        ])
+def get_admin1_name(country_code, admin1_code):
+    if admin1_code and admin1_code in admin1_names[country_code]:
+        return admin1_names[country_code][admin1_code]
     else:
-        # We don't have the admin1_code, we can't include it in the display_name
-        return ", ".join([
-            row["name"],
-            row["country_code"]
-        ])
+        # The city claims to have an admin1_code and country_code combination that isn't in
+        # admin1_names. I don't know what to do with it so let's just say it has no admin1_code.
+        print("admin1_code " + row["admin1_code"] + " not found for country " + row["country_code"])
+        return None
+
+# Counts cities with and without admin1 for each country. For most countries,
+# either all cities have admin1 or none of them do. This is good for a
+# consistent experience for the user. But, there are exceptions. I'm not sure
+# what if anything to do about it, but I at least want to keep track.
+admin1_code_counts = namedtuple("admin1_code_counts", ["with_admin1_count", "no_admin1_count"])
+country_admin1_stats = defaultdict(lambda : admin1_code_counts(0, 0))
+def update_country_admin1_stats(city_id):
+    with_admin1_count, no_admin1_count = country_admin1_stats[city_id.country]
+
+    if city_id.admin1:
+        with_admin1_count += 1
+    else:
+        no_admin1_count += 1
+
+    country_admin1_stats[city_id.country] = admin1_code_counts(with_admin1_count, no_admin1_count)
+
+def print_country_admin1_stats():
+    print ("List of countries where all of the admin1 codes are missing")
+    for country_code, admin1_code_counts in country_admin1_stats.items():
+        if admin1_code_counts.with_admin1_count == 0:
+            print(country_code, admin1_code_counts)
+    print ("List of countries where some (but not all) of the admin1 codes are missing")
+    for country_code, admin1_code_counts in country_admin1_stats.items():
+        if admin1_code_counts.no_admin1_count != 0 and admin1_code_counts.with_admin1_count != 0:
+            print(country_code, admin1_code_counts)
+
+def make_city_identifier(row):
+    """ Turn a city row into a `city_identifier`, which uniquely identifies a city"""
+    return city_identifier(
+        name=row["name"],
+        admin1=get_admin1_name(row["country_code"], row["admin1_code"]),
+        country=row["country_code"],
+
+        # admin2=...
+        #
+        # When we're ready: We could choose to add it to an entry only when we need it for deduping
+        # purposes. Or, we could always include it and let the front end hide it from search results
+        # if it's not needed for deduping, but show it when they click on the Marker. However, I
+        # don't know if the front end would easily be able to tell if it's needed for deduping, so
+        # we should probably mark it somehow here. A "dedupe" field that could say "admin2", "admin1",
+        # "country", or even "city". Whatever is the lowest level of specificity it needs to dedupe.
+    )
+
+def make_city(city_id, row):
+    city = {
+        "lat": row["latitude"],
+        "lon": row["longitude"],
+        "pop": row["population"],
+        **city_id._asdict()
+    }
+    if not city['admin1']:
+        del city['admin1']
+    return city
+
+def get_token_prefixes(city_id, prefix_length):
+    """For a given city, give me all the prefixes that will eventually become
+    file names that the city will go into"""
+
+    # TODO - sub-prefix_length names?
+    # TODO - sub-prefix_length partial matches - with high population cutoff, AND exact matches for tokens... but only if all the tokens are short? hmm.
+    # TODO - Also sure maybe we do enforce a min-length ("du", "of" ? not useful) unless there are no long words in the thing! Or something.
+    token_prefixes = set()
+
+    # All of these components will be searchable, even if not with the same priority
+    for name_component in [city_id.name, city_id.admin1 or "", city_id.country]:
+        # We hope to clean and split the searchable components the same way as in the
+        # search engine. We only care about that here because we want to make
+        # sure the correct file comes up when the user starts typing a name.
+
+        # Normalize each character
+        cleaned_name_component = u"".join([
+            c for c
+            in unicodedata.normalize('NFKD', name_component)
+            if not unicodedata.combining(c)
+        ]).lower()
+
+        # Split into tokens (words-ish) and get each prefix
+        token_prefixes |= {
+            t[:prefix_length] for t
+            in regex.split('[^\p{L}]+', cleaned_name_component)
+        }
+    return token_prefixes
 
 def write_cities(cities):
     TMP_OUTPUT_DIR = "output"
@@ -149,40 +185,17 @@ def write_cities(cities):
 
     outfile_data = defaultdict(list)
 
-    for display_name, properties in cities.items():
-        entry = {
-            "lat": properties["latitude"],
-            "lon": properties["longitude"],
-            "pop": properties["population"],
-            'name': display_name,
-        }
+    for city_id, entry in cities.items():
 
         prefix_length = 3
 
-        # We hope to clean and split the display name the same way as in the
-        # search engine. We only care about that here because we want to make
-        # sure the file names are correct. But, that does affect which file
-        # name comes up.
-        cleaned_display_name = u"".join([
-            c for c
-            in unicodedata.normalize('NFKD', display_name)
-            if not unicodedata.combining(c)
-        ]).lower()
-        token_prefixes = {
-            t[:prefix_length] for t
-            in regex.split('[^\p{L}]+', cleaned_display_name)
-        }
-
-        # TODO - sub-prefix_length names?
-        # TODO - sub-prefix_length partial matches - with high population cutoff, AND exact matches for tokens... but only if all the tokens are short? hmm.
-        # TODO - Also sure maybe we do enforce a min-length ("du", "of" ? not useful) unless there are no long words in the thing! Or something.
-        for prefix in token_prefixes:
+        for prefix in get_token_prefixes(city_id, prefix_length):
             outfile_data[prefix].append(entry)
 
     for prefix, entries in outfile_data.items():
         with gzip.open(os.path.join(TMP_OUTPUT_DIR, f"{prefix}.json.gz"), "wt") as outfile:
             outfile.write(json.dumps(entries, indent=2))
-    with gzip.open(os.path.join(TMP_OUTPUT_DIR, f"index_metadata.json.gz"), "wt") as outfile:
+    with gzip.open(os.path.join(TMP_OUTPUT_DIR, "index_metadata.json.gz"), "wt") as outfile:
         outfile.write(json.dumps({"stopwords": [""], "token_length": 3}, indent=2))
 
     tar = tarfile.open("static-search.2025-12-10.pop-1k-cities.tar.gz", "w:gz")
@@ -195,31 +208,28 @@ def write_cities(cities):
 # * Gather all of the cities in that country and pick one with a "median" location (want to pick one to make sure it's in the country in question)
 # TODO admin1s too, why not. "Minnesota".
 
-admin1_mapping = get_admin1_mapping()
+admin1_names = fetch_admin1_names()
 zip_data = BytesIO(requests.get("https://download.geonames.org/export/dump/cities1000.zip").content)
 
 with zipfile.ZipFile(zip_data, "r") as zf:
     csv_f = TextIOWrapper(zf.open("cities1000.txt", "r"), encoding='utf-8')
     reader = csv.reader(csv_f, delimiter='\t')
-    cities = defaultdict(dict)
 
+    cities = {}
     for row_ in reader:
         row = CitiesRow(row_)
 
-        assert_city_has_country(row)
-        clean_admin1_code_for_city(row)
-        display_name = city_display_name(row)
+        validate_city_row(row)
+        city_id = make_city_identifier(row)
+        update_country_admin1_stats(city_id)
 
-        if display_name not in cities or cities[display_name]['population'] < row["population"]:
-            cities[display_name] = {
-                "population": row["population"],
-                "latitude": row["latitude"],
-                "longitude": row["longitude"],
-            }
-        else:
-            print(
-                "Dupe city/country/admin1_code: " + display_name +
-                " populations: " + row["population"] + " vs " + cities[display_name]["population"]
-            )
+        if city_id in cities:
+            pop1 = row["population"]
+            pop2 = cities[city_id]["pop"]
+            print(f"Dupe city_id: {city_id} populations: {pop1} vs {pop2}")
 
+        if city_id not in cities or int(cities[city_id]["pop"]) < int(row["population"]):
+            cities[city_id] = make_city(city_id, row)
+
+    print_country_admin1_stats()
     write_cities(cities)
