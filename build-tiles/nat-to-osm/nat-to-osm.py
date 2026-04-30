056e41cefdf5a0bed1e8319aa7452a3cb02ff9d0
Daniel Krol <orblivion@gmail.com>
2026-04-29 21:27:03 -0400

    Fix comment in generated code

--- build-tiles/nat-to-osm/nat-to-osm.py
+++ build-tiles/nat-to-osm/nat-to-osm.py
@@ -25,7 +25,7 @@ def print_correction(val_a, val_b, obj_a, obj_b, trail):
     obj_path = ''.join(index_syntax(key) for key in trail)
 
     print (f"if (style{parent_obj_path}.id === '{obj_b['id']}') {{")
-    print (f"    style{obj_path} = {val_b} # from {val_a}")
+    print (f"    style{obj_path} = {val_b} // from {val_a}")
     print ( "} else {")
     print ( "    console.log(UNEXPECTED_ID_ERROR)")
     print ( "}")
